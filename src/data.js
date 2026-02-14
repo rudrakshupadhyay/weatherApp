d640e53bbfa4b865748bb1e7b0a150627775af3e
Rudraksh Upadhyay <218912408+rudrakshupadhyay@users.noreply.github.com>
2026-02-14 15:30:40 +0530

    code for fetching weather data

---
+++ src/data.js
@@ -0,0 +1,35 @@
+const key = '0cb2a94d8372941766303c654da36058';
+
+async function coordinate(city) {
+  const response = await fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`
+  );
+  const data = await response.json();
+
+  if (!data.length) {
+    throw new Error('City not found');
+  }
+
+  return {
+    glat: data[0].lat,
+    glon: data[0].lon,
+  };
+}
+
+async function weatherData() {
+  const { glat, glon } = await coordinate('Delhi');
+
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${glat}&lon=${glon}&appid=${key}`
+  );
+
+  if (!response.ok) {
+    throw new Error('Weather fetch failed');
+  }
+
+  return response.json();
+}
+
+weatherData()
+  .then((data) => console.log(data.weather))
+  .catch((err) => console.error(err.message));
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './data.js';
+console.log('hello');
+function add(num) {
+  return num + 1;
+}
